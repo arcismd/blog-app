43e751e1e7a33c6df3fa2b6d5d5ec12ad409ff14
Artur <a.sincarenco.97@gmail.com>
2022-08-04 18:18:16 +0300

    feat: connect to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express();
+const app = express()
+dotenv.config()
 
-app.listen(5000, () => {
-    console.log('Server started');
-});
+const PORT = process.env.PORT || 3001
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_NAME = process.env.DB_NAME
+
+async function start() {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster1.nslb6te.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+        )
+
+        app.listen(PORT, () => console.log(`Start server ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
